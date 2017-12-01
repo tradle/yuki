4b4d967415b41a490c9d84da8571a9cf43f233c3
Mark Vayngrib <mark@tradle.io>
2017-11-30 20:29:40 -0500

    fix: no need to validate built in models

--- models.js
+++ models.js
@@ -2,8 +2,8 @@ const baseModels = require('@tradle/models').models
 const customModels = require('@tradle/custom-models')
 const mergeModels = require('@tradle/merge-models')
 const models = mergeModels()
-  .add(baseModels)
-  .add(customModels)
+  .add(baseModels, { validate: false })
+  .add(customModels, { validate: false })
   .get()
 
 module.exports = models
